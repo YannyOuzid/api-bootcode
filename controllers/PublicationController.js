700be6db64d806eaf7949422e7e23895fc56b9a0
yannyiim <yanny.ouzid@edu.devinci.fr>
2021-12-10 14:58:42 +0100

    add delete, get and patch in publication route

--- controllers/PublicationController.js
+++ controllers/PublicationController.js
@@ -3,7 +3,7 @@ const Publication = require("../models/Publication");
 
 module.exports = {
     post: async (req, res) => {
-        let publi = new Publication({title: req.body.title, html: req.body.html, css: req.body.css, javascripti: req.body.javascript});
+        let publi = new Publication({title: req.body.title, html: req.body.html, css: req.body.css, javascript: req.body.javascript});
 
         try {
             const post = await publi.save();
@@ -12,4 +12,60 @@ module.exports = {
             res.json({message: err});
         }
     },
+
+    getAll: async (req, res) => {
+       try {
+           const publications = await Publication.find();
+           res.json(publications);
+       } catch(err) {
+           res.json({ message: err});
+       }
+    },
+
+    getOne: async (req,res) => {
+        try {
+            const publication = await Publication.findById(req.params.postId);
+            res.json(publication);
+        } catch (err) {
+            res.json({message: err});
+        }
+        
+    },
+
+    update: async (req,res) => {
+        try {
+            const post = await Publication.findOne({ _id: req.params.postId })
+
+            if (req.body.title) {
+                post.title = req.body.title;
+            }
+
+            if (req.body.html) {
+                post.html = req.body.html;
+            }
+
+            if (req.body.css) {
+                post.css = req.body.css;
+            }
+
+            if (req.body.javascript) {
+                post.javascript = req.body.javascript;
+            }
+
+            await post.save();
+            res.json(post);
+        } catch(err) {
+            res.json({ message: err});
+        }
+    },
+
+    delete: async (req,res) => {
+        try {
+            const post = await Publication.deleteOne({_id: req.params.postId});
+            res.json(post);
+        } catch(err) {
+            res.json({ message: err});
+        }
+    }
+
 }
--- routes/Publication.js
+++ routes/Publication.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const controller = require("../controllers/PublicationController");
 
 router.post('/post', controller.post);
-
+router.get('/', controller.getAll);
+router.get('/:postId', controller.getOne);
+router.patch('/:postId', controller.update);
+router.delete('/:postId', controller.delete);
 
 module.exports = router;
