9521d016e75e9fedacb33584ed3600eefeb119b0
yannyiim <yanny.ouzid@edu.devinci.fr>
2022-01-09 17:54:17 +0100

    add cors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors")
 const mongoose = require('mongoose');
 const url = "mongodb+srv://admin:admin@cluster0.vfeta.mongodb.net/bootcode?retryWrites=true&w=majority";
 const userRoutes = require("./routes/User");
 const publicationRoutes = require("./routes/Publication");
 
 app.use(express.json());
-
+app.use(cors())
 
 app.get('/', function (req, res) {
     res.send('API Bootcode')
