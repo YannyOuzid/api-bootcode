4ae035562cfdd0cbf0488a34b1afc4458ef77646
yannyiim <yanny.ouzid@edu.devinci.fr>
2022-01-11 18:56:40 +0100

    add publication and user one to many relation

--- routes/Publication.js
+++ routes/Publication.js
@@ -4,7 +4,7 @@ const controller = require("../controllers/PublicationController");
 
 router.post('/:userId/post', controller.post);
 router.get('/', controller.getAll);
-router.get('/:userId', controller.getUserPublication);
+router.get('/:userId/post', controller.getUserPublication);
 router.get('/:postId', controller.getOne);
 router.patch('/:postId', controller.update);
 router.delete('/:postId', controller.delete);
