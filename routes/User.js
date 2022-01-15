0c4205a172bd39cd3959084063b7112b6c5a1a7d
yannyiim <yanny.ouzid@edu.devinci.fr>
2022-01-16 00:04:19 +0100

    add new user route and tags for publication

--- routes/User.js
+++ routes/User.js
@@ -4,6 +4,6 @@ const controller = require("../controllers/UserController");
 
 router.post('/register', controller.register);
 router.post('/login', controller.login);
-router.get('/userId', controller.getOne);
+router.get('/:userId', controller.getOne);
 
 module.exports = router;
